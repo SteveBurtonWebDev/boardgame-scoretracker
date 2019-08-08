bf9d3627c76b71bf5f9db8a34cf0fa44d7cd8fd0
Steve Burton <steveburtonwebdev@gmail.com>
2019-08-08 10:13:17 +0100

    Updates

---
+++ precache-manifest.047353d16ffd4d20ac73d31b37418a76.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaaf1fa74b5dadb42913859bd1a6c303",
+    "url": "/boardgame-scoretracker/index.html"
+  },
+  {
+    "revision": "fb24603e792b5e190ecd",
+    "url": "/boardgame-scoretracker/static/css/main.ab7ba21f.chunk.css"
+  },
+  {
+    "revision": "d2103c5e095edc1346e1",
+    "url": "/boardgame-scoretracker/static/js/2.a68ccf2f.chunk.js"
+  },
+  {
+    "revision": "fb24603e792b5e190ecd",
+    "url": "/boardgame-scoretracker/static/js/main.5246062b.chunk.js"
+  },
+  {
+    "revision": "61fdb69e3239c6dd7394",
+    "url": "/boardgame-scoretracker/static/js/runtime~main.a986adb3.js"
+  },
+  {
+    "revision": "e0e558cd3017f70ce6dc2afbff572252",
+    "url": "/boardgame-scoretracker/static/media/hakseity.e0e558cd.jpg"
+  },
+  {
+    "revision": "262d8f477d8006c5cf2ba74224f76f4e",
+    "url": "/boardgame-scoretracker/static/media/life.262d8f47.png"
+  },
+  {
+    "revision": "0671567e92bf927a582a4a7e76283721",
+    "url": "/boardgame-scoretracker/static/media/mastery.0671567e.png"
+  }
+]);
